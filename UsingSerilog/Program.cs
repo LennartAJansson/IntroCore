a7cc51875c671c795cf3cb0925e91d83f7b67613
Lennart Jansson <lennart.jansson@sigma.se>
2021-02-10 14:26:56 +0100

    Removed sample for KeyVault

--- UsingSerilog/Program.cs
+++ UsingSerilog/Program.cs
@@ -6,13 +6,14 @@ using Serilog;
 
 namespace UsingSerilog
 {
-    static class Program
+    internal static class Program
     {
         private static void Main(string[] args) =>
             CreateHostBuilder(args).Build().Run();
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+
                 .UseSerilog((context, config) =>
                     config.ReadFrom.Configuration(context.Configuration))
 
